6f5cfe9c29f728018ae28c447a222decbf508221
hereticslayer <stanislawloter@gmail.com>
2022-11-10 23:34:20 +0100

    Working on the comparing algorythm, some errors to repair

--- index.php
+++ index.php
@@ -31,28 +31,55 @@ function toJson($file){
 }
 
 
-
-class Transaction{
-
-	function __construct(){
+// Mincer() should take $contents, merge its parts wherever it is needed, and return shaped, shiny array ready to print!
+function Mincer($contents) {
 		
-			$transaction -> time = strtotime($content[1]);
-			$transaction -> type = $content[3];
+	$transactions = [];
+	foreach ($contents as $content){
+
+	class Transaction
+	{
+
+		function __construct(){
+		
+			$this -> time = strtotime($content[1]);
+			$this -> type = $content[3];
 			if ($content[5]<0){
-				$transaction -> sell_currency = $content[4];
-				$transaction -> sell = 0 - $content[5];
+				
+				$this -> sell_currency = $content[4];
+				$this -> sell = 0 - $content[5];
 			} else {
-			$transaction -> buy_currency = $content[4];
-			$transaction -> buy = $content[5];
-			}	
-		
+			$this -> buy_currency = $content[4];
+			$this -> buy = $content[5];
+		}	
+	}
+	$object = new Transaction;
+	// Something is wrong with $object in line above, need to work on this one
+	$transactions[] = $object;
+
+}
+
+foreach ($transactions as $transaction){
+	$time = $transaction -> time;
+	$type = $transaction -> type;
+	$current_transaction = current($transaction);
+	if ($type === "Buy" || $type === "Sell"){
+		foreach($transactions as $transaction){
+			$time_2 = $transaction -> time;
+			$type_2 = $transaction -> type;
+			$other_transaction = current($transaction);
+			if ($time === $time_2 && ($type_2 === "Buy" || $type_2 === "Sell")){
+			$merged_object = (object) array_merge((array)$current_transaction, (array)$other_transaction);
+			// I need these two messages for testing
+			echo "Objects merged!/n";
+			}else{
+				echo "These object cannot be merged!/n";
+			}
+		}
 	}
 }
-$transactions = [];
-foreach ($contents as $content){
-	$transactions[] = new Transaction();
-	}
-	// The problem is: even if I could launch this, how am I going to refer to these new objects? They'd have no given name.
+}
+
 
 
 ?>
