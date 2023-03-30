16b3fea9e628ed19c9ad26249f219b6c3d5860a7
zloter <zygmuntloter@gmail.com>
2023-03-30 22:10:45 +0200

    My simple solution:

--- index.php
+++ index.php
@@ -1,223 +1,225 @@
-
 <?php
 
-$contents = toJson($argv[1]);
-
-// toJson read the data as the csv file and push it into array $contents
-function toJson($file){
-	//getting data
-	$open = fopen($file,"r+");
-
-	//case of wrong file
-	if(!$open){
-		die("Wrong file!");
-	}
-
-	// pushing data into $contents
-	while($data = fgetcsv($open, 1024, ",")){
-		$contents[] = $data;
-	}
-
-	// Returning
-	return $contents;
-}
-
-
-
-// Extracting headers from contents
-	$headers = array_shift($contents);
 
 date_default_timezone_set('Europe/Berlin');
 
+$fileName = $argv[1];
+
+$rows = readCSV($fileName);
+
+// Remove headers from rows
+array_shift($rows);
+
+$transactions = mapRawDataToTransactions($rows);
+
+$transactions = linkBuyAndSellTogether($transactions);
+
+usort($transactions, function ($a, $b){
+    return $a->time - $b->time;
+});
+
+$transactions = adjustOrderByType($transactions);
+
+printToJson($transactions);
+
+
+
 class Transaction {
-	public int $time;
-	public string $type;
-	public string $sell_currency;
-	public float $sell;
-	public string $buy_currency;
-	public float $buy;
+    public int $time;
+    public string $type;
+    public string $sell_currency;
+    public float $sell;
+    public string $buy_currency;
+    public float $buy;
 
+    public function isBuyType(): bool {
+        return $this->type === "Buy";
+    }
+
+    public function isSellType(): bool {
+        return $this->type === "Sell";
+    }
+
+    public function isBuy(): bool {
+        return isset($this->buy);
+    }
 }
 
-// mincer() should take $contents and shape its subarrays into objects
-function mincer($contents) {
+/**
+ * @param string $fileName
+ * @return array
+ */
+function readCSV(string $fileName): array {
+    //getting data
+    $file = fopen($fileName,"r+");
 
-	foreach ($contents as $content){
-	$object = new Transaction();
-	$object -> time = strtotime($content[1]);
-	if ($content[3] === "Fee") {
-		$object -> type = "Other fee";
-	} else {
-	$object -> type = $content[3];
-	}
-	if ($content[5] < 0){
-		$object -> sell_currency = $content[4];
-		$object -> sell = 0 - $content[5];
+    //case of wrong file
+    if(!$file){
+        die("Wrong file!");
+    }
 
-	} else {
-			$object -> buy_currency = $content[4];
-			$object -> buy = $content[5];
-	}
+    // pushing data into $contents
+    while($data = fgetcsv($file, 1024, ",")){
+        $lines[] = $data;
+    }
 
-		$transactions[] = $object;
+    fclose($file);
 
-	}
-	return $transactions;
+    // Returning
+    return $lines;
 }
 
-$transactions = mincer($contents);
+class Cols {
+    const TIME = 1;
+    const TYPE = 3;
+    const CURRENCY = 4;
+    const AMOUNT  = 5;
+}
 
+/**
+ * @param array $records
+ * @return array []Transaction
+ */
+function mapRawDataToTransactions(array $records): array {
+    $types = [
+        "Fee" => "Other fee",
+        "Super BNB Mining" => "Mining",
+        "Referral Kickback" => "Reward/Bonus",
+    ];
+    foreach ($records as $line){
+        $tx = new Transaction();
+        $tx->time = strtotime($line[Cols::TIME]);
+        $tx->type = $types[$line[Cols::TYPE]] ?? $line[Cols::TYPE];
 
+        if ($line[Cols::AMOUNT] < 0){
+            $tx -> sell_currency = $line[Cols::CURRENCY];
+            $tx -> sell = abs($line[Cols::AMOUNT]);
 
-// merge() should take arrays of object, merge the proper ones, and remove leftovers
+        } else {
+            $tx -> buy_currency = $line[Cols::CURRENCY];
+            $tx -> buy = $line[Cols::AMOUNT];
+        }
 
-function merge($transactions)
-{
-	$transactions_merged = [];
-		while (count($transactions)){
-		$transaction = array_shift($transactions);
-		if ($transaction->type === "Buy" || $transaction->type === "Sell"){
-			$merged = false;
-			foreach($transactions as $key => $pairable){
+        $transactions[] = $tx;
 
-				if (isMergable($transaction, $pairable)) {
-					$transactions_merged[]= mergeTransactions($transaction, $pairable);
-						//echo "Objects merged!\n";
-					unset($transactions[$key]);
-					$merged = true;
-					break;
-				}
-			}
-			if ($merged === false ) {
-                print_r($transactions_merged);
-                throw new Exception("without a match");
-				}
-		} else {
-			//echo "it has no type required to merge\n";
-			$transactions_merged[] = $transaction;
-		}
+    }
+    return $transactions;
+}
 
-	}
-	return $transactions_merged;
+/**
+ * @param array $transactions []Transaction
+ * @return array []Transaction
+ */
+function linkBuyAndSellTogether(array $transactions): array {
+    $result = [];
+    $buyFromBuyTypeTxs = [];
+    $sellFromBuyTypeTxs = [];
+    $buyFromSellTypeTxs = [];
+    $sellFromSellTypeTxs = [];
+
+    /** @var Transaction $transaction */
+    foreach($transactions as $transaction) {
+        if ($transaction->isBuyType() || $transaction->isSellType()) {
+
+            if ($transaction->isBuyType()) {
+                if ($transaction->isBuy()) {
+                    $buyFromBuyTypeTxs[] = $transaction;
+                } else {
+                    $sellFromBuyTypeTxs[] = $transaction;
+                }
+            } else if ($transaction->isSellType()) {
+                if ($transaction->isBuy()) {
+                    $buyFromSellTypeTxs[] = $transaction;
+                } else {
+                    $sellFromSellTypeTxs[] = $transaction;
+                }
+            }
+
+            if (count($buyFromBuyTypeTxs) > 0 && count($sellFromBuyTypeTxs) > 0) {
+                $result[] = mergeTransactions(array_shift($buyFromBuyTypeTxs), array_shift($sellFromBuyTypeTxs));
+            }
+
+            if (count($buyFromSellTypeTxs) > 0 && count($sellFromSellTypeTxs) > 0) {
+                $result[] = mergeTransactions(array_shift($buyFromSellTypeTxs), array_shift($sellFromSellTypeTxs));
+            }
+
+        } else {
+            $result[] = $transaction;
+        }
+    }
+
+    if ((count($buyFromBuyTypeTxs) + count($sellFromBuyTypeTxs) + count($buyFromSellTypeTxs) + count($sellFromSellTypeTxs)) > 0) {
+        throw new Exception("Some buy /sell transactions does not have a counterpart");
+    }
+    return $result;
 }
 
 
-function isMergable(Transaction $t1, Transaction $t2) {
-	$result = false;
-	if ($t1->time !== $t2->time ){
-		//echo "Different time!\n";
-		$result = false;
-	} elseif ($t1->type !== $t2->type){
-		//echo "Different type!\n";
-		$result = false;
-	} elseif (isset($t1->buy) && isset($t2->buy)){
-		//echo "Can't be both buy!\n";
-		$result = false;
-	} elseif (isset($t1->sell) && isset($t2->sell)){
-		//echo "Can't be both sell!\n";
-		$result = false;
-	} else {
-		$result = true;
-	}
-	return $result;
-}
 function mergeTransactions(Transaction $t1, Transaction $t2)
 {
-	if (isset($t1->buy)) {
-		$t1->sell = $t2->sell;
-		$t1->sell_currency = $t2->sell_currency;
-	} else {
-		$t1->buy = $t2->buy;
-		$t1->buy_currency = $t2->buy_currency;
-	}
-	$t1->type = "Trade";
-	return $t1;
+    $t1->sell = $t2->sell;
+    $t1->sell_currency = $t2->sell_currency;
+
+    $t1->type = "Trade";
+    return $t1;
 }
 
-$transactions_merged = merge($transactions);
+
+function sortBatch(array $currentStack): array {
+    $trades = [];
+    $fees = [];
+    $rest = [];
+    $result = [];
 
 
+    foreach ($currentStack as $element){
+        if ($element->type === 'Trade'){
+            $trades [] = $element;
+        } elseif ($element->type === 'Other fee') {
+            $fees [] = $element;
+        } else {
+            $rest [] = $element;
+        }
+    }
 
-function sorter($a, $b){
-	return $a->time < $b->time;
+
+    while((count($trades) + count($fees)) > 0) {
+        if (count($trades)) {
+            $result []= array_shift($trades);
+        }
+        if (count($fees)) {
+            $result []= array_shift($fees);
+        }
+    }
+
+    $result = array_merge($result, $rest);
+
+
+    return $result;
 }
-usort ($transactions_merged, "sorter");
 
-
-function funkySort($arr1,$arr2,$arr3)
+function adjustOrderByType(array $txs)
 {
-	$semifinal = [];
+    $currentStack = [];
+    $result = [];
 
-foreach ($arr1 as $obj1){
-	foreach ($arr2 as $obj2){
-		array_push($semifinal, $obj1, $obj2);
-	}
+    $txTime = -1;
+    foreach($txs as $key => $tx) {
+        if (($txTime !== $tx->time)) {
+            $result = array_merge($result, sortBatch($currentStack));
+            $currentStack = [];
+        }
+        $txTime = $tx->time;
+        $currentStack[] = $tx;
+    }
+    $result = array_merge($result, sortBatch($currentStack));
+
+    return $result;
 }
 
-	$result = array_merge($semifinal,$arr3);
-	$result = array_filter($result);
-	return $result;
-}
-
-function sortingHat($transactions_merged)
- {
-	 //array with results
-	$transactions_sorted = [];
-	//temporary arrays - categories, current stack
-	$trades = [];
-	$fees = [];
-	$rest = [];
-	$current_stack = [];
-	$sorted = [];
-
-	while(count($transactions_merged)){
-		$first_element = array_shift($transactions_merged);
-		$current_stack [] = $first_element;
-
-		while(count($transactions_merged)){
-			$second_element = array_shift($transactions_merged);
-
-			if ($first_element->time === $second_element->time){
-				$current_stack [] = $second_element;
-			} else {
-					foreach ($current_stack as $element){
-						if ($element->type === 'Trade'){
-							$trades [] = $element;
-						} elseif ($element->type === 'Other fee') {
-							$fees [] = $element;
-						} else {
-							$rest [] = $element;
-						}
-					}
-					$sorted [] = funkySort($trades, $fees, $rest);
-					$trades = [];
-					$fees = [];
-					$rest = [];
-					$current_stack = [];
-
-					}
-
-
-			}
-
-		}
-		$transactions_sorted = array_filter($sorted);
-		return $transactions_sorted;
-	}
-
-
-	$transactions_sorted = sortingHat($transactions_merged);
 
 
 function printToJson($transactions_sorted)
 {
-
-	foreach($transactions_sorted as $object) {
-		$values = array_values($object);
-	echo json_encode($values, JSON_PRETTY_PRINT);
-	}
+    echo json_encode($transactions_sorted, JSON_PRETTY_PRINT);
 }
-
- printToJson($transactions_sorted);
-
-?>
